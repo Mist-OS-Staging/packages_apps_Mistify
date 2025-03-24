c6227d09dfc4577aac316cbd84d9c9aea4d03709
ShukakuZa <yusufzuzu787@gmail.com>
2025-03-24 21:47:59 +0000

    mistify: revert: Introduce notification custom corner radius [2/2]

--- src/com/rising/settings/fragments/QuickSettings.java
+++ src/com/rising/settings/fragments/QuickSettings.java
@@ -35,7 +35,6 @@ import com.android.settingslib.search.SearchIndexable;
 import com.android.internal.util.android.ThemeUtils;
 
 import com.android.settings.preferences.CustomSeekBarPreference;
-import com.android.settings.preferences.SystemSettingSeekBarPreference;
 
 import com.android.settings.utils.SystemRestartUtils;
 
@@ -60,8 +59,6 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     private CustomSeekBarPreference mTileAnimationDuration;
     private Preference mSplitShadePref;
 
-    private SystemSettingSeekBarPreference mNotificationCornerRadius;
-
     private ThemeUtils mThemeUtils;
 
     private Handler mHandler = new Handler();
@@ -84,9 +81,6 @@ public class QuickSettings extends SettingsPreferenceFragment implements
 
         mSplitShadePref = (Preference) findPreference("qs_split_shade_enabled");
         mSplitShadePref.setOnPreferenceChangeListener(this);
-
-        mNotificationCornerRadius = (SystemSettingSeekBarPreference) findPreference("notification_corner_radius");
-         mNotificationCornerRadius.setOnPreferenceChangeListener(this);
 
         mTileAnimationStyle = (ListPreference) findPreference(KEY_PREF_TILE_ANIM_STYLE);
         mTileAnimationDuration = (CustomSeekBarPreference) findPreference(KEY_PREF_TILE_ANIM_DURATION);
@@ -128,13 +122,6 @@ public class QuickSettings extends SettingsPreferenceFragment implements
                    "qs_split_shade_enabled", value, UserHandle.USER_CURRENT);
             updateSplitShadeEnabled(getActivity());
             return true;
-        } else if (preference.getKey().equals("notification_corner_radius")) {
-             int newRadius = (int) newValue;
-             Settings.System.putIntForUser(resolver,
-                     "notification_corner_radius", newRadius, UserHandle.USER_CURRENT);
- 
-             new Handler().postDelayed(() -> SystemRestartUtils.restartSystemUI(getContext()), 75);
-             return true;
         }
         return false;
     }
