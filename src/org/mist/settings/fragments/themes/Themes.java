f0600cdd93d80611f3ba8111157f2e147c18af2e
ShukakuZa <yusufzuzu787@gmail.com>
2026-07-12 22:08:26 +0530

    Mistify: Cleanup for CP2A

--- src/org/mist/settings/fragments/themes/Themes.java
+++ src/org/mist/settings/fragments/themes/Themes.java
@@ -36,13 +36,13 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 
-import org.mist.settings.fragments.themes.SmartPixels;
+//import org.mist.settings.fragments.themes.SmartPixels;
 import org.mist.settings.utils.SystemUtils;
 import org.mist.settings.preferences.SystemSettingListPreference;
 import org.mist.settings.preferences.SystemPropertySwitchPreference;
 import org.mist.settings.preferences.SystemSettingSwitchPreference;
 
-import com.android.internal.util.mist.VibrationUtils;
+//import com.android.internal.util.mist.VibrationUtils;
 import com.android.internal.util.mist.ThemeUtils;
 
 import com.android.internal.util.mist.SystemRestartUtils;
@@ -55,20 +55,7 @@ public class Themes extends SettingsPreferenceFragment implements
 
     public static final String TAG = "UserInterface";
 
-    private static final String KEY_FORCE_FULL_SCREEN = "display_cutout_force_fullscreen_settings";
-    private static final String KEY_VOLUME_DIALOG_TYPE = "volume_dialog_type";
-    private static final String KEY_QUICKSWITCH = "quickswitch";
-    private static final String KEY_SHOW_VOLUME_PERCENTAGE = "show_volume_percentage";
-    private static final String KEY_IOS_VOLUME_EXPAND_ON_KEY = "ios_volume_expand_on_key";
-    private static final String SYS_ANI_OVERRIDE_ENABLED = "persist.sys.activity_anim_perf_override";
-
-    private Preference mShowCutoutForce;
-    private Preference mQuickSwitch;
-    private SystemSettingListPreference mVolumeDialogType;
-    private SystemSettingSwitchPreference mShowVolumePercentage;
-    private SystemSettingSwitchPreference mIosVolumeExpand;
     private ThemeUtils mThemeUtils;
-    private SystemPropertySwitchPreference mAniOverrideEnabled;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -80,41 +67,7 @@ public class Themes extends SettingsPreferenceFragment implements
         final PreferenceScreen prefScreen = getPreferenceScreen();
 
         mThemeUtils = ThemeUtils.getInstance(getActivity());
-
-        final String displayCutout =
-            mContext.getResources().getString(com.android.internal.R.string.config_mainBuiltInDisplayCutout);
-
-        if (TextUtils.isEmpty(displayCutout)) {
-            mShowCutoutForce = (Preference) findPreference(KEY_FORCE_FULL_SCREEN);
-            prefScreen.removePreference(mShowCutoutForce);
-        }
-
-        mQuickSwitch = (Preference) prefScreen.findPreference(KEY_QUICKSWITCH);
-        boolean withGoogleApps = android.os.SystemProperties.getBoolean("with_google_apps", false);
-        if (!withGoogleApps)
-            prefScreen.removePreference(mQuickSwitch);
-
-        mAniOverrideEnabled = (SystemPropertySwitchPreference) findPreference(SYS_ANI_OVERRIDE_ENABLED);
-        mAniOverrideEnabled.setOnPreferenceChangeListener(this);
-
-        mVolumeDialogType = findPreference(KEY_VOLUME_DIALOG_TYPE);
-        if (mVolumeDialogType != null) {
-            mVolumeDialogType.setOnPreferenceChangeListener(this);
-        }
-
-        mShowVolumePercentage = findPreference(KEY_SHOW_VOLUME_PERCENTAGE);
-        mIosVolumeExpand = findPreference(KEY_IOS_VOLUME_EXPAND_ON_KEY);
-        updateVolumePercentageVisibility();
-    }
-
-    private void updateVolumePercentageVisibility() {
-        if (mShowVolumePercentage == null) return;
-        int type = Settings.System.getIntForUser(
-                getContext().getContentResolver(),
-                KEY_VOLUME_DIALOG_TYPE, 1,
-                UserHandle.USER_CURRENT);
-        mShowVolumePercentage.setVisible(type == 1);
-        if (mIosVolumeExpand != null) mIosVolumeExpand.setVisible(type == 3);
+    
     }
 
     @Override
@@ -128,19 +81,6 @@ public class Themes extends SettingsPreferenceFragment implements
 
             int val = Integer.parseInt((String) newValue);
 
-            if (mShowVolumePercentage != null) {
-                mShowVolumePercentage.setVisible(val == 1);
-            }
-
-        if (mIosVolumeExpand != null) {
-            mIosVolumeExpand.setVisible(val == 3);
-        }
-
-        return true;
-    }
-
-    if (preference == mAniOverrideEnabled) {
-        SystemRestartUtils.showSystemRestartDialog(getContext());
         return true;
     }
         return false;
@@ -166,18 +106,6 @@ public class Themes extends SettingsPreferenceFragment implements
                 public List<String> getNonIndexableKeys(Context context) {
                     List<String> keys = super.getNonIndexableKeys(context);
 
-                    final String displayCutout =
-                        context.getResources().getString(com.android.internal.R.string.config_mainBuiltInDisplayCutout);
-
-                    if (TextUtils.isEmpty(displayCutout)) {
-                        keys.add(KEY_FORCE_FULL_SCREEN);
-                    }
-
-                    boolean withGoogleApps = android.os.SystemProperties.getBoolean("with_google_apps", false);
-                    if (!withGoogleApps)
-                        keys.add(KEY_QUICKSWITCH);
-
-                    return keys;
                 }
             };
 }
